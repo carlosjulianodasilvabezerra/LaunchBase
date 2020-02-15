4a916a9cbf8fbf9a66a8aa29eb7ec69e1a44d13d
johnabis <joao.abilio@yahoo.com>
2020-02-15 20:10:31 -0300

    updated challenge 4.3 included

--- modulo.4/Desafios/Desafios.4-1to7/routes.js
+++ modulo.4/Desafios/Desafios.4-1to7/routes.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const routes = express.Router()
+const teacher = require('./teacher')
 
 routes.get('/', function(req, res){
   return res.redirect('/teachers')
 })
+
+routes.get('/teachers/create', function(req, res){
+  return res.render('teachers/create')
+})
+
+routes.post('/teachers', teacher.post)
 
 routes.get('/teachers', function(req, res){
   return res.render('teachers/index')
